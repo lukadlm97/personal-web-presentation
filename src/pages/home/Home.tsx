50f6ba35a741c900f8cf0107e95cf5e048586679
luka.radovanovic@teletrader.com <luka.radovanovic@teletrader.com>
2022-09-17 23:58:44 +0200

    bug fix

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -47,40 +47,43 @@ function Home(){
               <Typography style={{fontFamily:'Montserrat, serif',fontWeight:'bold'}}>
                 Meet me
               </Typography>
-                <Button size="small"  style={{background:'#446145',color:'#8FE3CF',marginRight:'120px',padding:'5px',margin:'5px'}} onClick={()=>onMenuItemClick('biography')}>
+              <Box >
+                
+                <Button size="small"  style={{background:'#446145',color:'#8FE3CF',padding:'5px',margin:'5px'}} onClick={()=>onMenuItemClick('biography')}>
                     <PersonSearchIcon/>
                     <Typography  sx={{ fontSize: 14 }} style={{marginLeft:'5px'}}>
                       Biography 
                     </Typography>
                 </Button>
-                <Button size="small"  style={{background:'#6c7a6c',color:'#8FE3CF',marginRight:'120px',padding:'5px',margin:'5px'}} onClick={()=>onMenuItemClick('expirience')}>
+                <Button size="small"  style={{background:'#6c7a6c',color:'#8FE3CF',padding:'5px',margin:'5px'}} onClick={()=>onMenuItemClick('expirience')}>
                     <WorkHistoryIcon/>
                     <Typography  sx={{ fontSize: 14 }} style={{marginLeft:'5px'}}>
                     Expirience
                     </Typography>
            
                 </Button>
-                <Button size="small"  style={{background:'#215921',color:'#8FE3CF',marginRight:'120px',padding:'5px',margin:'5px'}} onClick={()=>onMenuItemClick('projects')}>
+                <Button size="small"  style={{background:'#215921',color:'#8FE3CF',padding:'5px',margin:'5px'}} onClick={()=>onMenuItemClick('projects')}>
                     <AssignmentIcon/>
                     <Typography  sx={{ fontSize: 14 }} style={{marginLeft:'5px'}}>
                     Projects
                     </Typography>
            
                 </Button>
-                <Button size="small"  style={{background:'#4a574a',color:'#8FE3CF',marginRight:'120px',padding:'5px',margin:'5px'}} onClick={()=>onMenuItemClick('hobbies')}>
+                <Button size="small"  style={{background:'#4a574a',color:'#8FE3CF',padding:'5px',margin:'5px'}} onClick={()=>onMenuItemClick('hobbies')}>
                     <SportsHandballIcon/>
                     <Typography  sx={{ fontSize: 14 }} style={{marginLeft:'5px'}}>
                       Hobbies
                     </Typography>
            
                 </Button>
-                <Button size="small"  style={{background:'#1e381e',color:'#8FE3CF',marginRight:'120px',padding:'5px',margin:'5px'}} onClick={()=>onMenuItemClick('recommendations')}>
+                <Button size="small"  style={{background:'#1e381e',color:'#8FE3CF',padding:'5px',margin:'5px'}} onClick={()=>onMenuItemClick('recommendations')}>
                     <GradeIcon/>
                     <Typography  sx={{ fontSize: 14 }} style={{marginLeft:'5px'}}>
                       Recommendations
                     </Typography>
            
                 </Button>
+              </Box>
             </Box>
            
            
